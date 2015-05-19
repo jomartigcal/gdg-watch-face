cb5aca96180c09165536b1f9ff76962b47db5311
Jomar Tigcal <jomar@tigcal.com>
2015-05-20 00:30:43 +0800

    Mobing status bar to top center
    Signed-off-by: Jomar Tigcal <jomar@tigcal.com>

--- wear/src/main/java/org/gdgph/watchface/GdgWatchFace.java
+++ wear/src/main/java/org/gdgph/watchface/GdgWatchFace.java
@@ -148,7 +148,7 @@ public class GdgWatchFace extends CanvasWatchFaceService {
                     .setShowSystemUiTime(false)
                     .setViewProtection(WatchFaceStyle.PROTECT_STATUS_BAR | WatchFaceStyle.PROTECT_HOTWORD_INDICATOR)
                     .setHotwordIndicatorGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL)
-                    .setStatusBarGravity(Gravity.CENTER | Gravity.LEFT)
+                    .setStatusBarGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL)
                     .setPeekOpacityMode(WatchFaceStyle.PEEK_OPACITY_MODE_TRANSLUCENT)
                     .build());
 
